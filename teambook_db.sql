b8b79bdb4f891a763deef49f3e366b125998bf05
afifun <moh.afifun46@gmail.com>
2015-05-01 08:10:03 +0700

    komit db baru

--- teambook_db.sql
+++ teambook_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 23 Apr 2015 pada 14.12
+-- Generation Time: 01 Mei 2015 pada 03.08
 -- Versi Server: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -31,17 +31,23 @@ CREATE TABLE IF NOT EXISTS `akun` (
   `nama` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `email` varchar(200) NOT NULL,
-  `isAdmin` int(11) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  `phone` varchar(15) NOT NULL,
+  `provinsi` varchar(200) NOT NULL,
+  `kota` varchar(200) NOT NULL,
+  `kode_pos` varchar(11) NOT NULL,
+  `alamat_shipping` varchar(1000) NOT NULL,
+  `alamat_default` varchar(1000) NOT NULL,
+  `isAdmin` tinyint(1) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 --
 -- Dumping data untuk tabel `akun`
 --
 
-INSERT INTO `akun` (`id`, `nama`, `password`, `email`, `isAdmin`) VALUES
-(1, 'afifun naily', 'afifun2193', 'moh.afifun46@gmail.com', 0),
-(2, 'admin', 'admin', 'admin@teambook.com', 1),
-(3, 'ulil', '1234', 'ulil@ulil.com', 0);
+INSERT INTO `akun` (`id`, `nama`, `password`, `email`, `phone`, `provinsi`, `kota`, `kode_pos`, `alamat_shipping`, `alamat_default`, `isAdmin`) VALUES
+(2, 'admin', 'admin', 'admin@teambook.com', '0', '', '', '0', '0', '', 1),
+(3, 'ulil', '1234', 'ulil@ulil.com', '0', '', '', '0', '0', '', 0),
+(11, 'afifun', '1234', 'afifun@gmail.com', '123123123', 'jawa barat', 'depok', '16424', 'Fakultas Ilmu Komputer Universitas Indonesia', 'Fakultas Ilmu Komputer Universitas Indonesia', 0);
 
 -- --------------------------------------------------------
 
@@ -53,20 +59,24 @@ CREATE TABLE IF NOT EXISTS `buku` (
 `id` int(11) NOT NULL,
   `isbn` varchar(100) NOT NULL,
   `judul` varchar(200) DEFAULT NULL,
+  `author` varchar(200) NOT NULL,
+  `penerbit` varchar(200) NOT NULL,
+  `publish_date` date NOT NULL,
   `kategori` varchar(100) DEFAULT NULL,
   `deskripsi` varchar(500) DEFAULT NULL,
   `gambar` text NOT NULL,
-  `harga` double NOT NULL
+  `harga` double NOT NULL,
+  `stok` int(11) NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data untuk tabel `buku`
 --
 
-INSERT INTO `buku` (`id`, `isbn`, `judul`, `kategori`, `deskripsi`, `gambar`, `harga`) VALUES
-(1, '19872019281', 'Bahasa Indonesia', 'Non Fiksi', 'Buku ini merupakan buku bahasa indonesia', 'images/19872019281.jpg', 48000),
-(2, '89798712018281', 'Matematika', 'Non Fiksi', 'Buku ini merupakan buku matematika', 'images/89798712018281.jpg', 75000),
-(3, '12987192837198', 'Laskar Pelangi', 'Fiksi', 'Buku ini merupakan buku Novel Laskar Pelangi', 'images/12987192837198.jpg', 90000);
+INSERT INTO `buku` (`id`, `isbn`, `judul`, `author`, `penerbit`, `publish_date`, `kategori`, `deskripsi`, `gambar`, `harga`, `stok`) VALUES
+(1, '19872019281', 'Bahasa Indonesia', 'afifun', 'afifun', '2015-04-10', 'Non Fiksi', 'Buku ini merupakan buku bahasa indonesia', 'images/buku/19872019281.jpg', 48000, 0),
+(2, '89798712018281', 'Matematika', 'adas', 'asdasd', '2015-04-15', 'Non Fiksi', 'Buku ini merupakan buku matematika', 'images/buku/89798712018281.jpg', 75000, 0),
+(3, '12987192837198', 'Laskar Pelangi', 'afifun', 'afifun', '2015-04-18', 'Fiksi', 'Buku ini merupakan buku Novel Laskar Pelangi', 'images/buku/12987192837198.jpg', 90000, 0);
 
 -- --------------------------------------------------------
 
@@ -79,15 +89,67 @@ CREATE TABLE IF NOT EXISTS `buku_keranjang` (
   `id_buku` int(11) NOT NULL,
   `id_keranjang` int(11) NOT NULL,
   `kuantitas` int(11) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=30 ;
 
 --
 -- Dumping data untuk tabel `buku_keranjang`
 --
 
 INSERT INTO `buku_keranjang` (`id`, `id_buku`, `id_keranjang`, `kuantitas`) VALUES
-(11, 1, 1, 1),
-(12, 2, 1, 2);
+(21, 1, 1, 5),
+(28, 1, 5, 1),
+(29, 2, 5, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku_order`
+--
+
+CREATE TABLE IF NOT EXISTS `buku_order` (
+`id` int(11) NOT NULL,
+  `id_buku` int(11) NOT NULL,
+  `id_order` int(11) NOT NULL,
+  `kuantitas` int(11) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+
+--
+-- Dumping data untuk tabel `buku_order`
+--
+
+INSERT INTO `buku_order` (`id`, `id_buku`, `id_order`, `kuantitas`) VALUES
+(11, 1, 1164539104, 2),
+(12, 2, 1164539104, 3),
+(13, 1, 1165268723, 2),
+(14, 2, 1165268723, 3),
+(15, 1, 1174794674, 1),
+(16, 2, 1174794674, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `checkout`
+--
+
+CREATE TABLE IF NOT EXISTS `checkout` (
+  `id` bigint(20) NOT NULL DEFAULT '123123',
+  `id_akun` int(11) NOT NULL,
+  `nama_kurir` varchar(100) NOT NULL,
+  `jenis_paket` varchar(100) NOT NULL,
+  `biaya_pengiriman` double NOT NULL,
+  `bank` varchar(100) NOT NULL,
+  `status` int(11) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `checkout`
+--
+
+INSERT INTO `checkout` (`id`, `id_akun`, `nama_kurir`, `jenis_paket`, `biaya_pengiriman`, `bank`, `status`, `tanggal`) VALUES
+(1164539104, 11, 'PenToLine', 'Paket Kilat', 25000, 'Danamon', 1, '2015-04-29'),
+(1165268723, 11, 'PenToLine', 'Paket Kilat', 25000, 'Danamon', 1, '2015-04-29'),
+(1174794674, 11, 'PenToLine', 'Paket Kilat', 15000, 'BNI', 1, '2015-04-29');
 
 -- --------------------------------------------------------
 
@@ -98,28 +160,16 @@ INSERT INTO `buku_keranjang` (`id`, `id_buku`, `id_keranjang`, `kuantitas`) VALU
 CREATE TABLE IF NOT EXISTS `keranjang` (
 `id_keranjang` int(11) NOT NULL,
   `id_akun` int(11) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data untuk tabel `keranjang`
 --
 
 INSERT INTO `keranjang` (`id_keranjang`, `id_akun`) VALUES
-(1, 1),
 (2, 2),
-(3, 3);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `order`
---
-
-CREATE TABLE IF NOT EXISTS `order` (
-`id_order` int(11) NOT NULL,
-  `id_akun` int(11) NOT NULL,
-  `id_buku` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+(3, 3),
+(5, 11);
 
 --
 -- Indexes for dumped tables
@@ -129,7 +179,7 @@ CREATE TABLE IF NOT EXISTS `order` (
 -- Indexes for table `akun`
 --
 ALTER TABLE `akun`
- ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `buku`
@@ -144,16 +194,22 @@ ALTER TABLE `buku_keranjang`
  ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `buku_order`
+--
+ALTER TABLE `buku_order`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `checkout`
+--
+ALTER TABLE `checkout`
+ ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `keranjang`
 --
 ALTER TABLE `keranjang`
  ADD PRIMARY KEY (`id_keranjang`);
-
---
--- Indexes for table `order`
---
-ALTER TABLE `order`
- ADD PRIMARY KEY (`id_order`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -163,7 +219,7 @@ ALTER TABLE `order`
 -- AUTO_INCREMENT for table `akun`
 --
 ALTER TABLE `akun`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT for table `buku`
 --
@@ -173,17 +229,17 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 -- AUTO_INCREMENT for table `buku_keranjang`
 --
 ALTER TABLE `buku_keranjang`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=30;
+--
+-- AUTO_INCREMENT for table `buku_order`
+--
+ALTER TABLE `buku_order`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT for table `keranjang`
 --
 ALTER TABLE `keranjang`
-MODIFY `id_keranjang` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `order`
---
-ALTER TABLE `order`
-MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id_keranjang` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
